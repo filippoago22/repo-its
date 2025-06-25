a403107c0e5a76cf223ee62b7e5a5899d64d910e
filippoago22 <filippo.agosti.14-2@accademiadigitaleliguria.it>
2025-06-25 15:34:19 +0200

    #1

---
+++ Progetto/ConsoleApp1/Calcolatrice_lb/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice_lb
+{
+    public class Calcolatrice
+    {
+        public double Somma(double x, double y)
+        {
+            return x+y;
+            //throw new NotImplementedException();
+
+        }
+    }
+
+   
+  
+}
---
+++ Progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Calcolatrice_lb;
+    namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            Calcolatrice_lb.Calcolatrice calcolatrice = new Calcolatrice_lb.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+        }
+    }
+}
